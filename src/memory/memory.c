ae6329269326768e15a8cba40a7b84b31d685781
Nurmyrza <bossnurmyrza@gmail.com>
2026-05-19 15:38:25 +0900

    implement function to load LC-3 programs into memory, handled big-endian to little-endian conversion

--- src/memory/memory.c
+++ src/memory/memory.c
@@ -2,6 +2,22 @@
 #include <stdlib.h>
 #include <string.h>
 
+/*******************************************************************************
+ ******************** PRIVATE UTILITY FUNCTION DECLARATIONS ********************
+ *******************************************************************************/
+
+/*
+    * swap each uint16_t type from big_endian to little-endian because almost 
+    * every modern computer uses little-endian
+    * @param x: the uint16_t type variable which we will swap 
+    * @return the little-endian version of the big_endian variable
+*/
+uint16_t swap16(uint16_t x);
+
+ /*******************************************************************************
+ *********************** PUBLIC FUNCTION IMPLEMENTATIONS ***********************
+ *******************************************************************************/
+
 uint16_t memory_read(uint16_t address, uint16_t* memory) {
     if (memory == NULL) {
         perror("Can't read from null memory reference");
@@ -23,4 +39,47 @@ int memory_write(uint16_t address, uint16_t val, uint16_t* memory) {
     }
     memory[address] = val;
     return EXIT_SUCCESS;
+}
+
+int loadfile(FILE* file, uint16_t* memory) {
+    if (memory == NULL) {
+        perror("Can't load a file in an unitialized memory!");
+        return EXIT_FAILURE;
+    }
+    uint16_t origin;
+    fread(&origin, sizeof(origin), 1, file);
+    origin = swap16(origin);
+
+    uint16_t* p = memory + origin;
+    size_t read = fread(p, sizeof(uint16_t), MEMORY_MAX - origin, file);
+    
+    while (read-- > 0) {
+        *p = swap16(*p);
+        ++ p;
+    }
+
+    return EXIT_SUCCESS;
+}
+
+int loadfile_from_path(const char* image_path, uint16_t* memory) {
+    if (image_path == NULL) {
+        perror("The given file path is not valid");
+        return EXIT_FAILURE;
+    }
+    FILE* file;
+    if ((file = fopen(image_path, "rb")) == NULL) {
+        perror("Error in opening the file!");
+        return EXIT_FAILURE;
+    }
+    loadfile(file, memory);
+    fclose(file);
+    return EXIT_SUCCESS;
+}
+
+/*******************************************************************************
+ ********************** PRIVATE FUNCTION IMPLEMENTATIONS ***********************
+ *******************************************************************************/
+
+uint16_t swap16(uint16_t x) {
+    return (x << 8) | (x >> 8);
 }
--- src/memory/memory.h
+++ src/memory/memory.h
@@ -8,18 +8,14 @@
     * RAM Memory is emulated through an uint16_t array with size MEMORY_MAX
     * set to the maximum size of the LC-3 System memory hardware (65536 btis)
 */
-
 #define MEMORY_MAX (1 << 16) 
 uint16_t memory[MEMORY_MAX];
-
-
 
 /*
     * Registers in the LC-3 ISA System. Enumeration is defined with constants 
     * whose numerical constants equal to the binary addresses of the registers
     * in a real LC-3 System.
 */
-
 typedef enum registers {
     R_R0,   /* Bin address: 0000*/
     R_R1,   /* Bin address: 0001*/
@@ -35,8 +31,6 @@ typedef enum registers {
 } registers_t;
 
 uint16_t reg[R_COUNT];
-
-
 
 /*
     * Returns the uint16_t data from the memory location at parameter "address"
@@ -54,5 +48,28 @@ uint16_t memory_read(uint16_t address, uint16_t* memory);
     @return EXIT_SUCCESS if successfuly written the data, EXIT_FAILURE otherwise
 */
 int memory_write(uint16_t address, uint16_t val, uint16_t* memory);
+
+/*
+    * Loads the given FILE stream into memory. File must be .obj file representing LC-3 program.
+    * FILE stream is read line-by-line, every instruction is saved in a corresponding address.
+    * In LC-3 ISA architecture, the first line of the program is the "origin" field containing the
+    * memory address value from which the program must be stored.
+    * In little-endian host architecture, we must swap to big-endian structure which LC-3 supports only.
+
+    * @param file: the file to be read
+    * @param memory: the LC-3 memory
+    * @return EXIT_SUCCESS if successful, otherwise EXIT_FAILURE
+*/
+int loadfile(FILE* file, uint16_t* memory);
+
+/*
+    * Opens FILE stream from given path and loads the stream into memory as in the loadfile method.
+    * @param image_path: the LC-3 program file path
+    * @param memory: the LC-3 system memory
+    * @return EXIT_SUCCESS if the operation is successful, EXIT_FAILURE if an error occurred into
+    * opening the stream from the path, in loading the stream into memory or if the file
+    * path was null
+*/
+int loadfile_from_path(const char* image_path, uint16_t* memory);
 
 #endif // MEMORY_H
